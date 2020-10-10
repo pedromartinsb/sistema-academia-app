7f66cc42beb2f9c69bf9c96b683ced4ad9729cf1
Pedro <engpedrocampos@gmail.com>
2020-10-10 11:36:49 -0300

    Novo chart

--- src/views/avaliacao-fisica/avaliacoesTable.js
+++ src/views/avaliacao-fisica/avaliacoesTable.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import AvaliacoesChart from './avaliacoesChart'
+var CanvasJSReact = require('../../assets/canvasjs.react')
+var CanvasJSChart = CanvasJSReact.CanvasJSChart
 
 export default props => {
 
@@ -30,6 +32,39 @@ export default props => {
         )
     })
 
+    const options = {
+        animationEnabled: true,
+        exportEnabled: true,
+        theme: "light2", //"light1", "dark1", "dark2"
+        title:{
+            text: "Simple Column Chart with Index Labels"
+        },
+        axisY: {
+            includeZero: true
+        },
+        data: [{
+            type: "column", //change type to bar, line, area, pie, etc
+            //indexLabel: "{y}", //Shows y value on all Data Points
+            indexLabelFontColor: "#5A5757",
+            indexLabelPlacement: "outside",
+            dataPoints: [
+                { x: 10, y: 71 },
+                { x: 20, y: 55 },
+                { x: 30, y: 50 },
+                { x: 40, y: 65 },
+                { x: 50, y: 71 },
+                { x: 60, y: 68 },
+                { x: 70, y: 38 },
+                { x: 80, y: 92, indexLabel: "Highest" },
+                { x: 90, y: 54 },
+                { x: 100, y: 60 },
+                { x: 110, y: 21 },
+                { x: 120, y: 49 },
+                { x: 130, y: 36 }
+            ]
+        }]
+    }
+
     /* const avaliacoesChart = props.avaliacoes.map( avaliacao => {
         return (
             <AvaliacoesChart avaliacao={avaliacao} />
@@ -57,7 +92,11 @@ export default props => {
             </tbody>
         </table>
 
-        {avaliacoesChart}
+        <div>
+			<CanvasJSChart options = {options} />
+		</div>
+
+        {/* {avaliacoesChart} */}
         </>
     )
 }
