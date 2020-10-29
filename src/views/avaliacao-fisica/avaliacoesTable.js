b9612f175288bef60afcaf2156d7daf98057e1bc
Pedro <engpedrocampos@gmail.com>
2020-10-29 19:18:19 -0300

    alteracao grafico

--- src/views/avaliacao-fisica/avaliacoesTable.js
+++ src/views/avaliacao-fisica/avaliacoesTable.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import { Chart } from "react-google-charts"
 
 export default props => {
 
+    const data = [
+        [
+            { type: 'number', label: 'id' },
+            { type: 'number', label: 'peso' },
+            { type: 'number', label: 'gordura corporal' }
+        ],
+    ];
+
+    var i = 1
+
     const rows = props.avaliacoes.map( avaliacao => {
-        console.log(avaliacao)
+
+        data.push([i, avaliacao.desempenho.peso, avaliacao.desempenho.gorduraCorporal])
+        i = i + 1
+
         return (
             <tr key={avaliacao.id}>
                 <td>{avaliacao.aluno.nome}</td>
                 <td>{avaliacao.avaliador}</td>
-                <td>{avaliacao.dataAvaliacao[2]}/{avaliacao.dataAvaliacao[1]}/{avaliacao.dataAvaliacao[0]}</td>
                 <td>{avaliacao.desempenho.altura}</td>
+                <td>{avaliacao.desempenho.peso}</td>
                 <td>{avaliacao.desempenho.gorduraCorporal}</td>
+                <td>{avaliacao.dataAvaliacao[2]}/{avaliacao.dataAvaliacao[1]}/{avaliacao.dataAvaliacao[0]}</td>         
                 <td>
                     <button type="button" 
                             className="btn btn-primary"
@@ -34,9 +49,10 @@ export default props => {
                 <tr>
                     <th scope="col">Nome</th>
                     <th scope="col">Avaliador</th>
-                    <th scope="col">Data Avaliação</th>
                     <th scope="col">Altura</th>
+                    <th scope="col">Peso</th>
                     <th scope="col">Gordura Corporal</th>
+                    <th scope="col">Data Avaliação</th>
                     <th scope="col">Ações</th>
                 </tr>
             </thead>
@@ -45,6 +61,20 @@ export default props => {
                 {rows}
             </tbody>
         </table>
+
+        <Chart
+            width="100%"
+            height="600px"
+            chartType="AreaChart"
+            loader={<div>Carregando gráfico</div>}
+            data={data}
+            options={{
+            title:
+            'Comparação Peso x Gordura',
+            intervals: { style: 'sticks' },
+            legend: 'none',
+            }}
+        />
         </>
     )
 }
